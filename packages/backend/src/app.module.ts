897b9a08b62a65c778a4fcc6ee82a8afbfd8f57b
asadeg02 <arezu.sadeghi@gmail.com>
2025-08-07 16:01:24 -0400

    cleanup app.module

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
-import { AppService } from './app.service';
-import { ResourceModule } from './resource/resource.module';
 import { SearchModule } from './search/search.module';
 import { DocumentMetadataModule } from './document-metadata/document-metadata.module';
 import { KeywordIndexModule } from './keyword-index/keyword-index.module';
@@ -10,7 +7,9 @@ import { KeywordIndexModule } from './keyword-index/keyword-index.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), 
-    ResourceModule, SearchModule, KeywordIndexModule, DocumentMetadataModule,
+    SearchModule,
+    KeywordIndexModule, 
+    DocumentMetadataModule
   ]
 })
 export class AppModule {}
