239f8ba87c26709f6f7230d532a7f0186e7ffb0b
asadeg02 <arezu.sadeghi@gmail.com>
2026-01-15 01:35:23 -0500

    feat: update subjects list

--- packages/frontend/src/constants/subjects.tsx
+++ packages/frontend/src/constants/subjects.tsx
@@ -1,14 +1,14 @@
 export const subjects = [
-    "Intersectional Resources",
-    "Coalition for Career Development Center",
-    "Gap Analysis/Loopholes",
-    "Career Exploration",
-    "Financial Literacy",
-    "Career Development Practices",
-    "SEL and Career Readiness",
-    "Individualized Learning Plans/PCAP",
-    "Community Engagement in Career Readiness\"&\"",
-    "Industry Engagement in Workforce Development",
-    "Career Counseling",
-    "International Resources"
+    'Career Counseling', 
+    'Career Development Practices', 
+    'SEL and Career Readiness', 
+    'Intersectional Resources', 
+    'Career Exploration', 
+    'Individualized Learning Plans/PCAP', 
+    'Coalition for Career Development Center', 
+    'Community Engagement in Career Readiness', 
+    'International Resources', 
+    'Industry Engagement in Workforce Development', 
+    'Financial Literacy'
 ];
+
